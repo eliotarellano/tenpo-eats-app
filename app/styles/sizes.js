f0463467a96434861f197f7a413b246fb4642c2a
eliotarellano <eliotarellano@MacBook-Pro-de-Eliot.local>
2020-08-14 19:51:07 -0400

    WelcomeScreen component ready to work

---
+++ app/styles/sizes.js
@@ -0,0 +1,12 @@
+// sizes constants
+export const sizes = {
+  h1: "4.5rem",
+  h2: "3.2rem",
+  h3: "2.7rem",
+  h4: "2.5rem",
+  h5: "1.6rem",
+  h6: "1.35rem",
+  body1: "1.13rem",
+  body2: "1rem",
+  small: "0.83rem",
+};
